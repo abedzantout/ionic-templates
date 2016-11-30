892c7412cba332886624ac543acfeaaf2418d27a
AliObaji <ali-obaji@hotmail.com>
2016-11-30 22:27:05 +0200

    Started work on a Modal with GoogleMaps for event location.

--- ionic-templates-7/src/pages/event-detail/event-detail.ts
+++ ionic-templates-7/src/pages/event-detail/event-detail.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController  ,NavParams} from 'ionic-angular';
 
+import { ModalController } from 'ionic-angular';
+import { ModalPage } from './modal-page';
+
+import { IconfigProvider } from '../../providers/iconfig-provider';
+
 /*
   Generated class for the EventDetail page.
 
@@ -14,7 +19,35 @@ import { NavController  ,NavParams} from 'ionic-angular';
 export class EventDetailPage {
   private Event: any;
 
-  constructor(public navCtrl: NavController, private NavParams:NavParams) {
+  private jsonContent: Object;
+
+  private showLocationButton: string;
+
+
+  constructor(public navCtrl: NavController, private NavParams:NavParams, private IconfigProvider: IconfigProvider,private modalCtrl: ModalController) {
+    //this function prepares the iconfig.json for parsing.
+    this.jsonContent = this.IconfigProvider.getJson().subscribe(
+      ( data ) => {this.jsonContent = data;},
+      ( err ) => {console.log(err);},
+      () => {
+        this.IconfigProvider.setJsonContent(this.jsonContent);
+        this.setJsonLocally();
+      }
+    );
     this.Event = this.NavParams.get('Object');
+  }
+
+  //this method sets the local variables of the component according to the iconfig file.
+  private setJsonLocally() {
+    this.jsonContent = JSON.parse(this.IconfigProvider.getJsonContent());
+    let content      = this.jsonContent[ 'Application' ][ 'page' ][ 2 ][ 'eventDetails' ];
+    let instance     = content[ 'default-instance' ];
+    this.showLocationButton = instance['showLocationButton'];
+  }
+
+  //this function opens the modal.
+  presentModal() {
+    let modal = this.modalCtrl.create(ModalPage);
+    modal.present();
   }
 }
